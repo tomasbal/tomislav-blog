730bc9becffb45247e2cb7a3ea75b5eb8f0d6abb
tomasbal <tomislav.balabanov@students.finki.ukim.mk>
2021-07-09 20:25:10 +0200

    Nav fix

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: 'https://tomislavbalabanov.me', title: 'Main Website' },
+  
 ]
 
 export default headerNavLinks
