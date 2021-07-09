e2fe4fa140573c1a6b6eb991d049ac75549107f9
tomasbal <tomislav.balabanov@students.finki.ukim.mk>
2021-07-09 23:46:32 +0200

    Colors fix 5

--- components/Tag.js
+++ components/Tag.js
@@ -4,7 +4,7 @@ import kebabCase from '@/lib/utils/kebabCase'
 const Tag = ({ text }) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="mr-3 text-sm font-medium text-red-500 uppercase hover:text-blue-600 dark:hover:text-blue-400">
+      <a className="mr-3 text-sm font-medium text-red-500 uppercase hover:text-red-600 dark:hover:text-red-600">
         {text.split(' ').join('-')}
       </a>
     </Link>
