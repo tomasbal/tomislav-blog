73ec8346ee9f78e9b3b67e831295b71c26663eee
tomasbal <tomislav.balabanov@students.finki.ukim.mk>
2021-07-10 17:23:47 +0200

    404 error page fix

--- pages/404.js
+++ pages/404.js
@@ -10,9 +10,9 @@ export default function FourZeroFour() {
       </div>
       <div className="max-w-md">
         <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-          Sorry we couldn't find this page.
+          Heeey bud, what are you doing here?
         </p>
-        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+        <p className="mb-8">As you can see this page does not exist, go to homepage instead.</p>
         <Link href="/">
           <button className="inline px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-red-600 border border-transparent rounded-lg shadow focus:outline-none focus:shadow-outline-red hover:bg-red-700 dark:hover:bg-red-500">
             Back to homepage
